249c7f7848dc45a5fcef56f1aceaa7a6d8bcf252
Gabriel Bach <gabrielcassolbach@gmail.com>
2023-09-21 10:16:42 -0300

    lab01-updated

--- Lab01/Lab01.vhd
+++ Lab01/Lab01.vhd
@@ -6,10 +6,10 @@ entity S4BIT7SEG is
 		SW_A: in std_logic_vector (3 downto 0);
 		Sw_B: in std_logic_vector (3 downto 0);
 		SW4: in std_logic;
-		HEX0: out std_logic_vector (0 to 6);
-		HEX2: out std_logic_vector (0 to 6);
-		HEX4: out std_logic_vector (0 to 6);
-		HEX5: out std_logic_vector (0 to 6)	
+		HEX0: out std_logic_vector (6 downto 0);
+		HEX2: out std_logic_vector (6 downto 0);
+		HEX4: out std_logic_vector (6 downto 0);
+		HEX5: out std_logic_vector (6 downto 0)	
 	);
 end S4BIT7SEG;
 
@@ -17,10 +17,11 @@ architecture struct of S4BIT7SEG is
 
 --signal
 signal SOMA_A: std_logic_vector (4 downto 0);
+signal B_OUT: std_logic_vector (3 downto 0); 
 --component 
 COMPONENT CONVERSOR7SEG 
 port (X: in std_logic_vector (3 downto 0);
-		SEG: out std_logic_vector (0 to 6));
+		SEG: out std_logic_vector (6 downto 0));
 end COMPONENT;
 
 --component
@@ -32,10 +33,14 @@ port(A: in std_logic_vector (3 downto 0);
 end COMPONENT;
 
 	begin 
-	--label: component_name  PORT_MAP  (port  list);
-conversorA: CONVERSOR7SEG port map (X => SW_A, SEG => HEX0);
-conversorB: CONVERSOR7SEG port map (X => Sw_B, SEG => HEX2);
-somador: 	SOMA4BITS 	  port map (A => SW_A, B => Sw_B, C_in => '0', S => SOMA_A); 
-conversorC: CONVERSOR7SEG port map (X => SOMA_A (3 downto 0), SEG => HEX4);
-conversorD: CONVERSOR7SEG port map (X => '0' & '0' & '0' & SOMA_A(4), SEG => HEX5);
+		B_OUT(0) <= SW4 xor SW_B(0);
+		B_OUT(1) <= SW4 xor SW_B(1);
+		B_OUT(2) <= SW4 xor SW_B(2);
+		B_OUT(3) <= SW4 xor SW_B(3);
+		--label: component_name  PORT_MAP  (port  list);
+		conversorA: CONVERSOR7SEG port map (X => SW_A, SEG => HEX0);
+		conversorB: CONVERSOR7SEG port map (X => Sw_B, SEG => HEX2);
+		somador: 	SOMA4BITS 	  port map (A => SW_A, B => B_OUT, C_in => SW4, S => SOMA_A); 
+		conversorC: CONVERSOR7SEG port map (X => SOMA_A (3 downto 0), SEG => HEX4);
+		conversorD: CONVERSOR7SEG port map (X => '0' & '0' & '0' & (SOMA_A(4) and not SW4), SEG => HEX5);
 	end struct;
