19dba1186b78f0200c4b482e40bb7efcbf19f28b
Gabriel Bach <gabrielcassolbach@gmail.com>
2023-10-03 09:50:48 -0300

    correction in sipo

--- Lab03/Lab03.vhd
+++ Lab03/Lab03.vhd
@@ -1,18 +1,19 @@
 Library IEEE;
 use ieee.std_logic_1164.all;
 
-entity Lab03 is
+entity Lab03_1 is
     port (
         pb0, pb1: in std_logic;
         sw: in std_logic_vector (7 downto 0);
         leds: out std_logic_vector (7 downto 0);
         hex0, hex1, hex4, hex5: out std_logic_vector (6 downto 0)
     );
-end Lab03;
+end Lab03_1;
 
-architecture struct of Lab03 is
+architecture struct of Lab03_1 is
 
 --signal
+signal rightClk: std_logic;
 signal registeredPiso: std_logic_vector (7 downto 0) := (others => '0');
 signal auxLeds: std_logic_vector (7 downto 0) := (others => '0');
 signal auxSerial: std_logic;
@@ -48,7 +49,7 @@ end component;
     begin 
     ---------------------------------------------------------------------------------------------
     piso: pisoConverter port map (sw_keys => sw, key0 => pb0, key1 => pb1, s_out => auxSerial);
-    sipo: sipoConverter port map (serial_in => auxSerial, key1 => pb1, q => auxLeds);
+	sipo: sipoConverter port map (serial_in => auxSerial, key1 => pb1, q => auxLeds);
     inputRegister: eightBitsRegister port map (rclk => pb0, d => sw, q => registeredPiso); 
     leds <= auxLeds;
     ---------------------------------------------------------------------------------------------
--- Lab03/sipoConverter.vhd
+++ Lab03/sipoConverter.vhd
@@ -12,8 +12,7 @@ end sipoConverter;
 architecture gate_level of sipoConverter is
 
 --signal
-signal auxq: std_logic_vector (7 downto 0);
-signal key1V: std_logic_vector (7 downto 0);
+signal auxq: std_logic_vector (7 downto 0) := (others => '0');
 
 --component 
 component jk_ff
@@ -24,13 +23,13 @@ end component;
     begin 
     --flip-flops:
     flip_flop1: jk_ff port map (j => serial_in, k => not serial_in, clk => key1, reset => '0', preset => '0', q => auxq(0));
-    flip_flop2: jk_ff port map (j => auxq(0) and (not key1), k => not (auxq(0) and (not key1)), clk => key1, reset => '0', preset => '0', q => auxq(1));
-    flip_flop3: jk_ff port map (j => auxq(1) and (not key1), k => not (auxq(1) and (not key1)), clk => key1, reset => '0', preset => '0', q => auxq(2));
-    flip_flop4: jk_ff port map (j => auxq(2) and (not key1), k => not (auxq(2) and (not key1)), clk => key1, reset => '0', preset => '0', q => auxq(3));
-    flip_flop5: jk_ff port map (j => auxq(3) and (not key1), k => not (auxq(3) and (not key1)), clk => key1, reset => '0', preset => '0', q => auxq(4));
-    flip_flop6: jk_ff port map (j => auxq(4) and (not key1), k => not (auxq(4) and (not key1)), clk => key1, reset => '0', preset => '0', q => auxq(5));
-    flip_flop7: jk_ff port map (j => auxq(5) and (not key1), k => not (auxq(5) and (not key1)), clk => key1, reset => '0', preset => '0', q => auxq(6));
-    flip_flop8: jk_ff port map (j => auxq(6) and (not key1), k => not (auxq(6) and (not key1)), clk => key1, reset => '0', preset => '0', q => auxq(7));
+    flip_flop2: jk_ff port map (j => auxq(0) and key1, k => not (auxq(0) and key1), clk => key1, reset => '0', preset => '0', q => auxq(1));
+    flip_flop3: jk_ff port map (j => auxq(1) and key1, k => not (auxq(1) and key1), clk => key1, reset => '0', preset => '0', q => auxq(2));
+    flip_flop4: jk_ff port map (j => auxq(2) and key1, k => not (auxq(2) and key1), clk => key1, reset => '0', preset => '0', q => auxq(3));
+    flip_flop5: jk_ff port map (j => auxq(3) and key1, k => not (auxq(3) and key1), clk => key1, reset => '0', preset => '0', q => auxq(4));
+    flip_flop6: jk_ff port map (j => auxq(4) and key1, k => not (auxq(4) and key1), clk => key1, reset => '0', preset => '0', q => auxq(5));
+    flip_flop7: jk_ff port map (j => auxq(5) and key1, k => not (auxq(5) and key1), clk => key1, reset => '0', preset => '0', q => auxq(6));
+    flip_flop8: jk_ff port map (j => auxq(6) and key1, k => not (auxq(6) and key1), clk => key1, reset => '0', preset => '0', q => auxq(7));
     --output:
     q <= auxq;
     end gate_level;
