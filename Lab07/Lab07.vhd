5f6bbf6fdc5d029f0822100358fe87e69fcef502
gabrielcassolbach <gabrielcassolbach@gmail.com>
2023-10-27 09:00:48 -0300

    Lab07-starter

---
+++ Lab07/Lab07.vhd
@@ -0,0 +1,22 @@
+-- Laboratório 07 -> Função principal.
+Library IEEE;
+use ieee.std_logic_1164.all;
+
+entity Lab07 is
+    port (
+
+    );
+end Lab07;
+
+
+architecture struct of Lab07 is
+
+--signals 
+
+--components
+
+    begin 
+
+
+
+    end struct;
